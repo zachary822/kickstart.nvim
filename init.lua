ac4522a272cde94a103f098dec81825dae6c6464
Zachary Juang <zachary822@me.com>
2025-02-14 19:56:40 -0500

    tweak

--- init.lua
+++ init.lua
@@ -978,6 +978,17 @@ require('lazy').setup({
   {
     'HiPhish/rainbow-delimiters.nvim',
   },
+  {
+    'hedyhli/outline.nvim',
+    config = function()
+      -- Example mapping to toggle outline
+      vim.keymap.set('n', '<leader>o', '<cmd>Outline<CR>', { desc = 'Toggle Outline' })
+
+      require('outline').setup {
+        -- Your setup opts here (leave empty to use defaults)
+      }
+    end,
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
