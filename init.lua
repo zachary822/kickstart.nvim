7855f8c7cb3500299d49868285b078131280f989
Zachary Juang <zachary@markups.ai>
2025-03-05 16:47:16 -0500

    update things

--- init.lua
+++ init.lua
@@ -169,6 +169,14 @@ vim.opt.rtp:prepend '/opt/homebrew/opt/fzf'
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
+--
+vim.keymap.set('n', ']t', function()
+  require('todo-comments').jump_next()
+end, { desc = 'Next todo comment' })
+
+vim.keymap.set('n', '[t', function()
+  require('todo-comments').jump_prev()
+end, { desc = 'Previous todo comment' })
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
@@ -629,7 +637,7 @@ require('lazy').setup({
         -- gopls = {},
         pyright = {},
         -- pylsp = {},
-        ruff = {},
+        -- ruff = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
