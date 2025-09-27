320e6aa5d419a82888cc0a725e359851f3517f9e
Zachary Juang <zachary822@me.com>
2025-09-27 06:14:42 -0400

    fix

--- init.lua
+++ init.lua
@@ -726,19 +726,27 @@ require('lazy').setup({
             },
           },
         },
+        hls = {},
+        zls = {},
+        elixirls = {},
+        clangd = {},
+        gopls = {},
+        ocamllsp = {},
+        racket_languageserver = {},
+        denols = {
+          root_dir = nvim_lsp.util.root_pattern('deno.json', 'deno.jsonc'),
+          single_file_support = false,
+        },
       }
 
-      nvim_lsp.hls.setup {}
-      nvim_lsp.zls.setup {}
-      nvim_lsp.elixirls.setup {}
-      nvim_lsp.clangd.setup {}
-      nvim_lsp.gopls.setup {}
-      nvim_lsp.ocamllsp.setup {}
-      nvim_lsp.racket_langserver.setup {}
-      nvim_lsp.denols.setup {
-        root_dir = nvim_lsp.util.root_pattern('deno.json', 'deno.jsonc'),
-        single_file_support = false,
-      }
+      -- The following loop will configure each server with the capabilities we defined above.
+      -- This will ensure that all servers have the same base configuration, but also
+      -- allow for server-specific overrides.
+      for server_name, server_config in pairs(servers) do
+        server_config.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server_config.capabilities or {})
+        vim.lsp.config(server_name, server_config)
+        vim.lsp.enable(server_name)
+      end
 
       -- Ensure the servers and tools above are installed
       --
@@ -757,22 +765,7 @@ require('lazy').setup({
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
       })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-      require('mason-lspconfig').setup {
-        ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-        automatic_installation = false,
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
-      }
+      -- require('mason-tool-installer').setup { ensure_installed = ensure_installed }
     end,
   },
 
